304c826a26a490bfe76fbda9acd4e865a7cb24f9
HemangVora <hemangvora2@gmail.com>
2023-04-10 13:40:55 +0530

    twitter test

---
+++ pages/cars/[id].js
@@ -0,0 +1,52 @@
+import Head from "next/head";
+import Image from "next/image";
+import { useRouter } from "next/router";
+
+export default function cars({ car }) {
+  const router = useRouter();
+  const { id } = router.query;
+  return (
+    <>
+      <Head>
+        <title>{id}</title>
+        <title>Piece - Turn Tweets into collectibles</title>
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="piece.gg" />
+        <meta
+          name="twitter:title"
+          content="Piece - Turn Tweets into collectibles"
+        />
+        <meta name="twitter:creator" content="piece.gg" />
+        <meta name="twitter:image" content={car.img1} />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://piece.gg" />
+        <meta
+          property="og:title"
+          content="Piece - Turn Tweets into collectibles"
+        />
+        <meta name="twitter:site" content="piece.gg" />
+        <meta property="og:description" content="__DESCRIPTION__" />
+        <meta property="og:image" content={car.img1} />
+      </Head>
+      <Image
+        src={car.img1}
+        alt="Picture of the author"
+        width={1000}
+        height={500}
+      />
+    </>
+  );
+}
+
+export async function getServerSideProps({ params }) {
+  const req = await fetch(
+    `http://localhost:8000/piece/img/${params.id}`
+  );
+
+  const data = await req.json();
+
+  return {
+    props: { car: data },
+  };
+}
